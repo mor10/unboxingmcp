5316d016d333293c5d79649b7e38977f8c37e69b
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2025-07-26 12:45:20 -0700

    update

--- day-5/weather-server-github-auth/src/index.ts
+++ day-5/weather-server-github-auth/src/index.ts
@@ -33,7 +33,6 @@ type Props = {
 const ALLOWED_USERNAMES = new Set<string>([
 	// Add GitHub usernames of users who should have access to the image generation tool
 	// For example: 'yourusername', 'coworkerusername'
-	'mor10'
 ]);
 
 export class MyMCP extends McpAgent<Env, Record<string, never>, Props> {
@@ -327,6 +326,38 @@ export class MyMCP extends McpAgent<Env, Record<string, never>, Props> {
 				};
 			}
 		);
+
+		// Dynamically add tools based on the user's login. In this case, I want to limit
+		// access to my Image Generation tool to just me
+		if (ALLOWED_USERNAMES.has(this.props.login)) {
+			this.server.tool(
+				"generateImage",
+				"Generate an image using the `flux-1-schnell` model. Works best with 8 steps.",
+				{
+					prompt: z
+						.string()
+						.describe("A text description of the image you want to generate."),
+					steps: z
+						.number()
+						.min(4)
+						.max(8)
+						.default(4)
+						.describe(
+							"The number of diffusion steps; higher values can improve quality but take longer. Must be between 4 and 8, inclusive.",
+						),
+				},
+				async ({ prompt, steps }) => {
+					const response = await this.env.AI.run("@cf/black-forest-labs/flux-1-schnell", {
+						prompt,
+						steps,
+					});
+
+					return {
+						content: [{ data: response.image!, mimeType: "image/jpeg", type: "image" }],
+					};
+				},
+			);
+		}
 
 	}
 }
